3d0f2cc34e0f722e36bdfb1f80c801a9914b5013
Lachee <lachlan.h@hotmail.com>
2021-03-10 20:14:09 +1100

    Added a webservice for postman to automatically sign requests while testing

--- dev/postman-signer.js
+++ dev/postman-signer.js
@@ -2,18 +2,24 @@ const http      = require('http');
 const crypto    = require('crypto');
 const fs        = require('fs');
 
-const publisherPrivateKey = "keys/publisher-private.key";
+const publisherPrivateKey = "keys/private.key";
+const port = 9494;
 
 const server = http.createServer(function(req, res) { 
+    console.log('processing request');
+
     let contents = '';
     req.on('data', chunk => { contents += chunk.toString(); });                
     req.on('end', () => { 
         const privateKey = fs.readFileSync(publisherPrivateKey);
         const sign = crypto.createSign('SHA256');
-        sign.write(stringified);
+        sign.write(contents);
         sign.end();
         const signature = sign.sign(privateKey, 'hex');
+    
+        console.log('Signature', contents, signature);
         res.end(signature);
     });
 });
-server.listen(9494);
+server.listen(port);
+console.log(`Listening to http://localhost:${port}`);
